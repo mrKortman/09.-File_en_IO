12b41e838f73749fd6775bc22dd3934cceff52d0
Boy Kortman <b.kortman@leeuwenborgh.nl>
2017-09-25 14:50:28 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.File_en_IO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
